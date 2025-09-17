5d9e043ef9c44bfe74c051044b3a9b2a3c70faea
alexoowl <asberesnev@gmail.com>
2025-09-17 13:34:21 +0200

    add view company_details

--- app_run/views.py
+++ app_run/views.py
@@ -2,13 +2,24 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.conf import settings
+
+# @api_view(['GET'])
+# def company_details_view(request):
+#     return Response({
+#         'company_name': 'Клуб дедов-бегунов',
+#         'slogan': 'Бегаем в любую погоду!',
+#         'contacts': 'Мы везде!'
+#         }, status=status.HTTP_200_OK)
+
 
 @api_view(['GET'])
-def company_details_view(request):
-    return Response({
-        'company_name': 'Клуб дедов-бегунов',
-        'slogan': 'Бегаем в любую погоду!',
-        'contacts': 'Мы везде!'
-        }, status=status.HTTP_200_OK)
+def company_details(request):
+    details = {'company_name': settings.COMPANY_NAME,
+               'slogan': settings.SLOGAN,
+               'contacts': settings.CONTACTS}
+    return Response(details)
+
+
 
 
