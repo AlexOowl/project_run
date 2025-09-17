f4202737b06bd6bf961f431ee7f1abc1e3cefcc9
alexoowl <asberesnev@gmail.com>
2025-09-17 15:04:36 +0200

    created serializers.py, add ViewSet and router

--- app_run/views.py
+++ app_run/views.py
@@ -3,6 +3,9 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.conf import settings
+from .serializers import RunSerializer
+from rest_framework import viewsets
+from .models import Run
 
 
 @api_view(['GET'])
@@ -12,6 +15,10 @@ def company_details(request):
                'contacts': settings.CONTACTS}
     return Response(details)
 
+
+class RunViewSet(viewsets.ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
 
 
 
