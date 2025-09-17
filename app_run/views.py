496422751e0459136befd18823b71e39141937d4
alexoowl <asberesnev@gmail.com>
2025-09-17 11:02:20 +0200

    first task

--- app_run/views.py
+++ app_run/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def company_details_view(request):
+    return Response({
+        'company_name': 'Клуб дедов-бегунов',
+        'slogan': 'Бегаем в любую погоду!',
+        'contacts': 'Мы везде!'
+        })
+
+
