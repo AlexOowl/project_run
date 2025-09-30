e6f8eb0c894be91df78a01de0bcb5ba2eaccf00d
alexoowl <asberesnev@gmail.com>
2025-09-30 15:02:39 +0200

    added DjangoFilterBackend and Pagination

--- app_run/views.py
+++ app_run/views.py
@@ -42,7 +42,8 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = UserSerializer
     filter_backends = [SearchFilter]
     search_fields = ['first_name', 'last_name']
-    filter_backends = ['date_joined']
+    filter_backends += ['date_joined']
+    ordering_fields = ['date_joined']
     pagination_class = UserPagination
 
     def get_queryset(self):
