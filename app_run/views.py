489df6f76471613c5bc36f6769be5c4f97aef9c5
alexoowl <asberesnev@gmail.com>
2025-09-30 12:53:47 +0200

    added DjangoFilterBackend and Pagination

--- app_run/views.py
+++ app_run/views.py
@@ -6,8 +6,10 @@ from django.conf import settings
 from .serializers import RunSerializer, UserSerializer
 from rest_framework import viewsets
 from .models import Run, User
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.views import APIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination
 
 
 
@@ -19,9 +21,21 @@ def company_details(request):
     return Response(details)
 
 
+class RunPagination(PageNumberPagination):
+    page_size_query_param = 'size'
+
+
+class UserPagination(PageNumberPagination):
+    page_size_query_param = 'size'
+
+
 class RunViewSet(viewsets.ModelViewSet):
     queryset = Run.objects.select_related('athlete').all()
     serializer_class = RunSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['status', 'athlete']
+    ordering_fields = ['created_at']
+    pagination_class = RunPagination
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
@@ -29,6 +43,8 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = UserSerializer
     filter_backends = [SearchFilter]
     search_fields = ['first_name', 'last_name']
+    filter_backends = ['date_joined']
+    pagination_class = UserPagination
 
     def get_queryset(self):
         qs = User.objects.filter(is_superuser=False)
