46ad923f0e288fc6d4e01982130a19df906b03d3
alexoowl <asberesnev@gmail.com>
2025-10-01 12:30:25 +0200

    added DjangoFilterBackend and Pagination

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -4,14 +4,17 @@ from .models import Run, User
 
 class UserSerializer(serializers.ModelSerializer):
     type = serializers.SerializerMethodField()
+    runs_finished = serializers.SerializerMethodField()
     class Meta:
         model = User
-        fields = ['id', 'date_joined', 'username' , 'last_name', 'first_name', 'type']
+        fields = ['id', 'date_joined', 'username' , 'last_name', 'first_name', 'type', 'runs_finished']
         # fields = ['id', 'username' , 'last_name', 'first_name']
 
     def get_type(self, obj):
         return 'coach' if obj.is_staff else 'athlete'
 
+    def get_runs_finished(self, obj):
+        return obj.run_set.filter(status='finished').count()
 
 
 class RunSerializer(serializers.ModelSerializer):
