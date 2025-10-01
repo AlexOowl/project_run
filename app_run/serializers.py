e033ef903cbbe473a5589a881690059a05c8d053
alexoowl <asberesnev@gmail.com>
2025-10-01 12:31:15 +0200

    added runs_finished to UserSerializer

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Run, User
 
 
+
 class UserSerializer(serializers.ModelSerializer):
     type = serializers.SerializerMethodField()
     runs_finished = serializers.SerializerMethodField()
