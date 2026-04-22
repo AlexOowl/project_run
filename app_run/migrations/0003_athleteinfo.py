2ecd0174848dcc23c55d0ae19b0b901ecd4867a2
alexoowl <asberesnev@gmail.com>
2026-04-22 12:38:21 +0200

    added athlete_info

--- app_run/migrations/0003_athleteinfo.py
+++ app_run/migrations/0003_athleteinfo.py
@@ -19,7 +19,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('goals', models.TextField()),
                 ('weight', models.IntegerField()),
-                ('user_id', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
     ]
